907c6e6771210bb6b5cec818ae7585094271ffc9
anotheranoob <k447038@gmail.com>
2017-09-18 11:44:03 -0400

    Update trololololol.py

--- trololololol.py
+++ trololololol.py
@@ -1,4 +1,12 @@
-for i in range(int(input("How much do you want to get trolled? input a integer"))):
+while True:
+    number=input("How much do you want to get trolled? input a integer")
+    try:
+        number=int(number)
+        if number<=10:
+            raise ValueError
+    except ValueError:
+        pass
+for i in range(number):
     file=open(str(i)+".txt", "w")
     print(str(i), file=file)
     file.close
